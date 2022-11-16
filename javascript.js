895a302b6566788898c898f2a4d37372d28b3eb2
Jackson <jtsoco@gmail.com>
2022-11-16 15:12:48 +0900

    get working grid etch a sketch, it's ugly though

--- javascript.js
+++ javascript.js
@@ -10,16 +10,26 @@ gridSelector.addEventListener('click', makeGrids);
 //and puts back out that number of divs
 function makeGrids() {
 
+    //add a function that will remove all
+    //of the child nodes before making a new grid
+
+
 let gridNumber = prompt("Grid side length please!", "4")
 let x = ((Number(gridNumber))**2);
 for (let i = 1; i <= x; i++) {
     const gridBox = document.createElement("div");
-    const content = document.createTextNode (i);
+    //const content = document.createTextNode (i);
 
-    gridBox.append(content);
-
+    //gridBox.append(content);
+    gridBox.className = "box";
 
     gridContainer.appendChild(gridBox);
+    gridBox.addEventListener('mouseover', changeBox)
+
+    
+   function changeBox() {
+gridBox.style.backgroundColor="black"
+}
 
 }
 gridSideCount = "";
@@ -28,7 +38,20 @@ for (let i = 1; i <= gridNumber; i++) {
 
 }
     
+
+
 gridContainer.style.gridTemplateColumns = gridSideCount;
+
+
+
+
+
 }
+//This changes all the grid boxes when hovered over
+
+
+
+
+
 
 
